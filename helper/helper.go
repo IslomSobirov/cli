af2391d08c0c6430bbb13b5fe6b5169b6d0312d5
IslomSobirov <atomm262@gmail.com>
2021-01-07 22:53:53 +0500

    Email send function added to helper

--- helper/helper.go
+++ helper/helper.go
@@ -2,7 +2,9 @@ package helper
 
 import (
 	"errors"
+	"fmt"
 	"log"
+	"net/smtp"
 	"os"
 	"regexp"
 )
@@ -27,4 +29,27 @@ func CheckEmail(email string) error {
 		return nil
 	}
 	return errors.New("Email is not correctly formated")
+}
+
+//SendEmail send mail
+func SendEmail(reciver []string, message []byte) {
+	// Sender data.
+	from := "notifyuseruzcom@gmail.com"
+	password := "thisIsFunToCreate0924"
+
+	// smtp server configuration.
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+
+	// Authentication.
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// Sending email.
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, reciver, message)
+	if err != nil {
+		LogError(err.Error())
+		log.Fatal(err)
+		return
+	}
+	fmt.Println("Email Sent Successfully!")
 }
