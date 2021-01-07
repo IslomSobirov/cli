9af5697db3deba030610be00babbc5af9f5c09b2
IslomSobirov <atomm262@gmail.com>
2021-01-08 00:03:45 +0500

    sms function has been written to helper

--- helper/helper.go
+++ helper/helper.go
@@ -1,13 +1,27 @@
 package helper
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"net/smtp"
 	"os"
 	"regexp"
+	"strings"
 )
+
+//SMSRequestBody struct for sms service
+type SMSRequestBody struct {
+	From      string `json:"from"`
+	Text      string `json:"text"`
+	To        string `json:"to"`
+	APIKey    string `json:"api_key"`
+	APISecret string `json:"api_secret"`
+}
 
 //LogError log the error to info.log
 func LogError(message string) {
@@ -52,4 +66,38 @@ func SendEmail(reciver []string, message []byte) {
 		return
 	}
 	fmt.Println("Email Sent Successfully!")
+}
+
+//SendSms send sms to clients
+func SendSms(message string, phoneNumber string) {
+	phoneNumber = strings.Replace(phoneNumber, "+", "", 1)
+	body := SMSRequestBody{
+		APIKey:    os.Getenv("NEXMO_API_KEY"),
+		APISecret: os.Getenv("NEXMP_API_SECRET"),
+		To:        phoneNumber,
+		From:      "Store",
+		Text:      message,
+	}
+
+	smsBody, err := json.Marshal(body)
+	if err != nil {
+		LogError(err.Error())
+		log.Fatal(err.Error())
+	}
+
+	resp, err := http.Post("https://rest.nexmo.com/sms/json", "application/json", bytes.NewBuffer(smsBody))
+	if err != nil {
+		LogError(err.Error())
+		log.Fatal(err.Error())
+	}
+
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		LogError(err.Error())
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println(string(respBody))
 }
